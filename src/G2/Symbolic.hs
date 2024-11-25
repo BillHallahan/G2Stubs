7c8001cb03eed5f8aac9a53480455dd53aca3394
Bill Hallahan <whallahan@binhamton.edu>
2024-11-25 16:00:17 -0500

    Added functions to G2.Symbolic

--- src/G2/Symbolic.hs
+++ src/G2/Symbolic.hs
@@ -1,4 +1,10 @@
-module G2.Symbolic (mkSymbolic) where
+module G2.Symbolic ( mkSymbolic
+                   , assume
+                   , assumeIO
+                   , assert
+                   , assertIO ) where
+
+-- import Unsafe.G2
 
 -- | Generates a fresh symbolic variable during symbolic execution
 mkSymbolic :: IO a
@@ -7,3 +13,17 @@ mkSymbolic = return symgen
 -- Hack to get a SymGen somewhere in base
 symgen :: a
 symgen = symgen
+
+{-# NOINLINE assume #-}
+assume :: Bool -> a -> a
+assume _ x = x
+
+assumeIO :: Bool -> IO ()
+assumeIO pred = assume pred (return ())
+
+assert :: Bool -> a -> a
+assert True x = x
+assert False _ = error "assert: assertion violation"
+
+assertIO :: Bool -> IO ()
+assertIO pred = assert pred (return ())
